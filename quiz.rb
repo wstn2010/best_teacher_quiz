603b8cf1423b44d05d83e46208f013b831279a64
kenputer <wstn2010@gmail.com>
2014-11-15 22:43:03 +0900

    fixed to make incorrect problem when given 1

--- quiz.rb
+++ quiz.rb
@@ -4,7 +4,7 @@ class Quiz
     
     def initialize(n)
         @n = n
-        @v = (1..n).to_a.shuffle
+        @v = n > 1 ? (1..n).to_a.shuffle : []
         @sol = v.delete_at(rand(n))
     end
 end
